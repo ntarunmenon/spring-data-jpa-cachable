27616eeba87f76108a758ea11c1bea9ca31e9074
Arun Menon <arun.menon@autogeneral.com.au>
2021-01-06 05:19:21 +1000

    Adding Caching to spring boot

--- src/main/java/org/arunm/springdatajpacachable/GreetingRepository.java
+++ src/main/java/org/arunm/springdatajpacachable/GreetingRepository.java
@@ -1,6 +1,13 @@
 package org.arunm.springdatajpacachable;
 
+import java.util.Optional;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GreetingRepository extends CrudRepository<Greeting, String> {
+
+    @Override
+    @Cacheable("GreetingsCache")
+    Optional<Greeting> findById(String s);
 }
--- src/test/java/org/arunm/springdatajpacachable/SpringDataJpaCachableApplicationTests.java
+++ src/test/java/org/arunm/springdatajpacachable/SpringDataJpaCachableApplicationTests.java
@@ -3,10 +3,14 @@ package org.arunm.springdatajpacachable;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,6 +23,9 @@ class SpringDataJpaCachableApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private CacheManager caffeineCacheManager;
+
     @Test
     void testGreeting() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/greeting?id=100")
@@ -26,6 +33,11 @@ class SpringDataJpaCachableApplicationTests {
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content", is(equalTo("Hello darkness"))));
+
+        Greeting cacheValue = caffeineCacheManager.getCache("GreetingsCache")
+                .get("100",Greeting.class);
+        Assertions.assertEquals(cacheValue.getContent(), "Hello darkness");
+
     }
 
 }
